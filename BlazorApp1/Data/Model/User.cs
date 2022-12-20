6e40073ba61546d206f55e9130c0aee70d8fcfda
Hansol Cho <zwwz11@naver.com>
2022-12-20 20:52:53 +0900

    초기 커밋.

---
+++ BlazorApp1/Data/Model/User.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp1.Data.Model
+{
+    public class User
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ BlazorApp1/Data/Service/UserService.cs
@@ -0,0 +1,15 @@
+﻿using BlazorApp1.Data.Model;
+
+namespace BlazorApp1.Data.Service
+{
+    public class UserService
+    {
+        private Dictionary<long, User> _users = new Dictionary<long, User>();
+        private long _id = 0;
+
+        public void SaveUser(User user)
+        {
+            _users.Add(_id++, user);
+        }
+    }
+}
